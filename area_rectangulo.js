89368ed95941e7345d9882ae5631b6d3c844d35e
AnJimenezM <136032517+AnJimenezM@users.noreply.github.com>
2023-06-24 12:39:45 -0500

    commit

---
+++ area_rectangulo.js
@@ -0,0 +1,7 @@
+
+
+var base = parseInt(prompt("ingresa la base"));
+      var altura = parseInt(prompt("ingresa la altura"));
+      var area
+      area = base * altura;
+      alert("el area del rectangulo es" + area);
